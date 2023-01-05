aa240d19643775ae9fe4b49508dfd4b9540783e6
sigmaone-eth <93957993+sigmaone-eth@users.noreply.github.com>
2023-01-05 13:59:58 -0500

    Update Task: Primitive Data Types.sol

--- Task: Primitive Data Types.sol
+++ Task: Primitive Data Types.sol
@@ -1,27 +1,31 @@
 pragma solidity ^ 0.8.0;   
    
-// Creating a contract
-contract Types {   
- 
-    // Initializing Bool variable
-    bool public boolean = false;
-    
-    // Initializing Integer variable
-    int32 public int_var = -60313;
- 
-    //  Initializing String variable
-    string public str = "GeeksforGeeks";
- 
-    // Initializing Byte variable
-    bytes1 public b = "a";
+//Signed integers: These are integers that can be either positive or negative. An example of a use case for signed integers is storing a count 
+//of something that can be both added and subtracted, such as the balance of a cryptocurrency account.
+
+int256 public a=-123;
+
+//Unsigned integers: These are integers that can only be positive. An example of a use case for unsigned integers is storing a count of something that can only be added,
+//such as the number of times a button has been clicked on a website.
+
+uint256 public a= 123;
+
+//Boolean: These are values that can either be true or false. An example of a use case for booleans
+//is storing a flag that indicates whether a certain condition is met.
+
+bool flag;
      
-    // Defining an enumerator
-    enum my_enum { geeks_, _for, _geeks } 
+//Addresses: These are values that represent the address of an Ethereum account. An example of a use case 
+//for addresses is storing the address of a contract that needs to be called.
  
-    // Defining a function to return
-    // values stored in an enumerator
-    function Enum() public pure returns(
-      my_enum) {   
-        return my_enum._geeks;   
-    }   
-}
+address public defaultAddr;
+ 
+//Enums: These are values that can be one of a predefined set of options. An example of a use case for enums is storing a status that can be one of 
+//a few different values, such as "pending", "approved", or "rejected".
+
+enum Status { Pending, Approved, Rejected }
+
+//Bytes: These are values that represent an array of bytes.
+//An example of a use case for bytes is storing a hash or a message.
+
+bytes32 hash;
